2cc6708f693ecea654c14aad43907d071aca9651
Idris Dopico Peña <dopico1996@hotmail.com>
2023-05-07 00:51:04 +0200

    Add some nice icons

--- src/navigation/components/TabNavigator.tsx
+++ src/navigation/components/TabNavigator.tsx
@@ -1,11 +1,14 @@
+import React from 'react';
 import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
 import { StatusBar } from 'expo-status-bar';
-import React from 'react';
+import { Ionicons } from "@expo/vector-icons";
+
 import Routes from '../routes';
 import Home from '../stacks/Home';
 import Anything from '../stacks/Anything';
 import Anywhere from '../stacks/Anywhere';
 import More from '../stacks/More';
+
 import theme from "../../theme";
 
 const Tab = createBottomTabNavigator();
@@ -31,18 +34,54 @@ const TabNavigator = () => {
         <Tab.Screen
           name={Routes.Home}
           component={Home}
+          options={{
+            tabBarIcon: (tabInfo) => (
+              <Ionicons
+                name="square-outline"
+                size={24}
+                color={tabInfo.focused ? theme.colors.white : theme.colors.whiteOpacity}
+              />
+            ),
+          }}
         />
         <Tab.Screen
           name={Routes.Anything}
           component={Anything}
+          options={{
+            tabBarIcon: (tabInfo) => (
+              <Ionicons
+                name="flower"
+                size={24}
+                color={tabInfo.focused ? theme.colors.white : theme.colors.whiteOpacity}
+              />
+            ),
+          }}
         />
         <Tab.Screen
           name={Routes.Anywhere}
           component={Anywhere}
+          options={{
+            tabBarIcon: (tabInfo) => (
+              <Ionicons
+                name="flower-outline"
+                size={24}
+                color={tabInfo.focused ? theme.colors.white : theme.colors.whiteOpacity}
+              />
+            ),
+          }}
         />
         <Tab.Screen
           name={Routes.More}
           component={More}
+          options={{
+            tabBarIcon: (tabInfo) => (
+              <Ionicons
+                name="beer"
+                size={24}
+                color={tabInfo.focused ? theme.colors.white : theme.colors.whiteOpacity}
+              />
+            ),
+          }}
         />
       </Tab.Navigator>
     </>
